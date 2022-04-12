115c29124fa7254b89a21bad6ca6d20df0a96384
Michael Griehm <griehmmj@gmail.com>
2022-04-12 15:55:31 -0400

    Added seperate data lake for synapse and added linked service for existing adls

--- iac/workload/data-lake.tf
+++ iac/workload/data-lake.tf
@@ -2,8 +2,3 @@ data "azurerm_storage_account" "adls" {
   name                = "cryptoanalyticslake"
   resource_group_name = "adls2-demo-eastus2"
 }
-
-resource "azurerm_storage_data_lake_gen2_filesystem" "synapse_workspace" {
-  name               = "synapse-workspace"
-  storage_account_id = data.azurerm_storage_account.adls.id
-}
--- iac/workload/synapse.tf
+++ iac/workload/synapse.tf
@@ -8,11 +8,26 @@ resource "random_password" "password" {
   override_special = "_%@"
 }
 
+resource "azurerm_storage_account" "synapsedatalake" {
+  name                     = "synapsedatalake"
+  resource_group_name      = data.azurerm_resource_group.rg.name
+  location                 = data.azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  is_hns_enabled           = "true"
+}
+
+resource "azurerm_storage_data_lake_gen2_filesystem" "synapse_root" {
+  name               = "synapse-root"
+  storage_account_id = azurerm_storage_account.synapsedatalake.id
+}
+
 resource "azurerm_synapse_workspace" "synapse" {
   name                                 = "cryptoanalyticssynapse"
   resource_group_name                  = data.azurerm_resource_group.rg.name
   location                             = data.azurerm_resource_group.rg.location
-  storage_data_lake_gen2_filesystem_id = azurerm_storage_data_lake_gen2_filesystem.synapse_workspace.id
+  storage_data_lake_gen2_filesystem_id = azurerm_storage_data_lake_gen2_filesystem.synapse_root.id
   sql_administrator_login              = "sqladminuser"
   sql_administrator_login_password     = random_password.password.result
   tags                                 = var.tags
@@ -65,4 +80,23 @@ resource "azurerm_synapse_role_assignment" "admin_role_assignment" {
   principal_id         = data.azuread_user.synapse_admin_user_account.object_id
 
   depends_on = [azurerm_synapse_firewall_rule.allow_all]
+}
+
+resource "azurerm_synapse_linked_service" "adls_linked_service" {
+  name                 = "adls-linked-service"
+  synapse_workspace_id = azurerm_synapse_workspace.synapse.id
+  type                 = "AzureBlobFS"
+  type_properties_json = <<JSON
+{
+            "url": "https://${data.azurerm_storage_account.adls.name}.dfs.core.windows.net", 
+            "accountkey": { 
+                "type": "SecureString", 
+                "value": "${data.azurerm_storage_account.adls.primary_access_key}" 
+            }
+        }
+JSON
+
+  depends_on = [
+    azurerm_synapse_firewall_rule.allow_all,
+  ]
 }
